d8f7a7b6f39183560e64d2e5675787a4a7911bf3
Justin Lin <caterpillar@openhome.cc>
2017-04-08 17:12:18 +0800

    added missing code

--- example/CH15/SomeThread.java
+++ example/CH15/SomeThread.java
@@ -3,12 +3,14 @@ package onlyfun.caterpillar;
 public class SomeThread implements Runnable {
     public void run() { 
         System.out.println("sleep....至 blocked 狀態"); 
-        try { 
-            Thread.sleep(9999); 
-        } 
-        catch(InterruptedException e) { 
-            System.out.println("I am interrupted...."); 
-        } 
+        while(true) {
+            try { 
+                Thread.sleep(9999); 
+            } 
+            catch(InterruptedException e) { 
+                System.out.println("I am interrupted...."); 
+            } 
+        }
     } 
 
     public static void main(String[] args) { 
